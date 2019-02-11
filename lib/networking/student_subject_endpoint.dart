203d0e0cdab2aa2f65085d68385c794c4bb50969
pmsdoliveira <pmsd.oliveira@campus.fct.unl.pt>
2019-02-11 16:53:21 +0000

    Clicking on a subject now also displays a list of enrolled students

--- lib/networking/student_subject_endpoint.dart
+++ lib/networking/student_subject_endpoint.dart
@@ -5,13 +5,26 @@ import 'package:http/http.dart' as http;
 import 'package:clip/config/variables.dart';
 
 Future<List<StudentSubjectsInfo>> fetchStudentSubjects(int studentID) async {
- final studentSufix = "student/" + studentID.toString() + "/info/";
+  final studentSufix = "student/" + studentID.toString() + "/info/";
 
   try {
     final response = await http.get(SERVER_ENDPOINT + studentSufix);
     return List<StudentSubjectsInfo>.from(json.decode(response.body).map((x) => StudentSubjectsInfo.fromJson(x)));
-  } catch(err) {
+  } catch (err) {
     print(err);
   }
+  return [];
+}
+
+Future<List<StudentSubjectsInfo>> fetchSubjectStudents(int subjectID) async {
+  final subjectSufix = "subject/" + subjectID.toString() + "/info/";
+
+  try {
+    final response = await http.get(SERVER_ENDPOINT + subjectSufix);
+    return List<StudentSubjectsInfo>.from(json.decode(response.body).map((x) => StudentSubjectsInfo.fromJson(x)));
+  } catch (err) {
+    print(err);
+  }
+
   return [];
 }
