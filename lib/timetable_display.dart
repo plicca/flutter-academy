b2e523140566938774cd398a4b01da9f74a8d574
ShaziaSulemane <shazia.sulemane@gmail.com>
2019-02-12 12:21:48 +0000

    added timetable file

---
+++ lib/timetable_display.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+
+class Timetable extends StatefulWidget {
+  @override
+  _Timetable createState() => new _Timetable();
+}
+
+class _Timetable extends State<Timetable> {
+
+}
